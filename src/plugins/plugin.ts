42b32bc617647607ad7eb6ce7288a5faae8df1b9
X-spec7 <mercury.spec77@gmail.com>
2025-10-14 20:47:39 +0000

    fix: fix prisma client patch mismatch

--- src/plugins/plugin.ts
+++ src/plugins/plugin.ts
@@ -1,5 +1,5 @@
 import fp from 'fastify-plugin'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma'
 
 declare module 'fastify' {
   interface FastifyInstance {
